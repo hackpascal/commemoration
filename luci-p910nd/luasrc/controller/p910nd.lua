db62016cc090cc29965fb94f41415dc981b97c93
hackpascal <hackpascal@qq.com>
2015-11-03 23:55:40 +0800

    openwrt-bb 中被舍弃的项目

---
+++ luci-p910nd/luasrc/controller/p910nd.lua
@@ -0,0 +1,27 @@
+--[[
+LuCI - Lua Configuration Interface
+
+Copyright 2008 Steven Barth <steven@midlink.org>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+$Id$
+]]--
+
+require("luci.sys")
+
+module("luci.controller.p910nd", package.seeall)
+
+function index()
+	if not nixio.fs.access("/etc/config/p910nd") then
+		return
+	end
+
+	local page
+
+	page = entry({"admin", "services", "p910nd"}, cbi("p910nd"), _("USB Printer Server"), 50)
+end
